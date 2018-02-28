591bd4ff0d822aea66a25ce051f8fa3ac7386fdc
Marcus Muench <marcus.muench@otto.de>
2018-02-28 16:48:31 +0100

    Implemented the recursive parameter instead of using our own paging implementation for getParametersByPath.

--- edison-aws-config/src/test/java/de/otto/edison/aws/config/paramstore/ParamStorePropertySourcePostProcessorTest.java
+++ edison-aws-config/src/test/java/de/otto/edison/aws/config/paramstore/ParamStorePropertySourcePostProcessorTest.java
@@ -65,6 +65,7 @@ public class ParamStorePropertySourcePostProcessorTest {
                 .builder()
                 .path("/the/path")
                 .withDecryption(true)
+                .recursive(true)
                 .build();
         final Properties propertiesSource = new Properties();
         propertiesSource.setProperty("/the/path/param1", "val1");
@@ -77,57 +78,6 @@ public class ParamStorePropertySourcePostProcessorTest {
 
         // then
         verify(ssmClient).getParametersByPath(eq(expectedRequest));
-        verify(propertySourcesMock).addFirst(parameterStorePropertySource);
-    }
-
-    @Test
-    public void shouldPageSsmResponse() {
-        // given
-        final GetParametersByPathResponse resultMock1 = mock(GetParametersByPathResponse.class);
-        final List<Parameter> parameters1 = new ArrayList<>();
-        parameters1.add(Parameter.builder().name("/the/path/param1").value("val1").build());
-        parameters1.add(Parameter.builder().name("/the/path/param2").value("val2").build());
-        when(resultMock1.parameters()).thenReturn(parameters1);
-        when(resultMock1.nextToken()).thenReturn("nextToken");
-
-        final GetParametersByPathResponse resultMock2 = mock(GetParametersByPathResponse.class);
-        final List<Parameter> parameters2 = new ArrayList<>();
-        parameters2.add(Parameter.builder().name("/the/path/param3").value("val3").build());
-        parameters2.add(Parameter.builder().name("/the/path/param4").value("val4").build());
-        when(resultMock2.parameters()).thenReturn(parameters2);
-        when(resultMock2.nextToken()).thenReturn("");
-
-        when(ssmClient.getParametersByPath(any(GetParametersByPathRequest.class))).thenReturn(resultMock1).thenReturn(resultMock2);
-        final ConfigurableEnvironment envMock = mock(ConfigurableEnvironment.class);
-        when(beanFactory.getBean(ConfigurableEnvironment.class)).thenReturn(envMock);
-        final MutablePropertySources propertySourcesMock = mock(MutablePropertySources.class);
-        when(envMock.getPropertySources()).thenReturn(propertySourcesMock);
-
-        final GetParametersByPathRequest expectedRequest1 = GetParametersByPathRequest
-                .builder()
-                .path("/the/path")
-                .withDecryption(true)
-                .build();
-        final GetParametersByPathRequest expectedRequest2 = GetParametersByPathRequest
-                .builder()
-                .path("/the/path")
-                .withDecryption(true)
-                .nextToken("nextToken")
-                .build();
-        final Properties propertiesSource = new Properties();
-        propertiesSource.setProperty("/the/path/param1", "val1");
-        propertiesSource.setProperty("/the/path/param2", "val2");
-        propertiesSource.setProperty("/the/path/param3", "val3");
-        propertiesSource.setProperty("/the/path/param4", "val4");
-        final PropertiesPropertySource parameterStorePropertySource =
-                new PropertiesPropertySource("parameterStorePropertySource", propertiesSource);
-
-        // when
-        postProcessor.postProcessBeanFactory(beanFactory);
-
-        // then
-        verify(ssmClient).getParametersByPath(eq(expectedRequest1));
-        verify(ssmClient).getParametersByPath(eq(expectedRequest2));
         verify(propertySourcesMock).addFirst(parameterStorePropertySource);
     }
 }
