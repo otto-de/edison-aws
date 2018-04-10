e87dd72d0f9d642ba06f26038a9f3a343776e418
Fabian Hoffmann <fabian.hoffmann@otto.de>
2018-04-10 16:20:59 +0200

    add paging to ssm properties loader

--- edison-aws-config/src/main/java/de/otto/edison/aws/config/paramstore/ParamStorePropertySourcePostProcessor.java
+++ edison-aws-config/src/main/java/de/otto/edison/aws/config/paramstore/ParamStorePropertySourcePostProcessor.java
@@ -17,9 +17,12 @@ import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.ssm.SSMClient;
 import software.amazon.awssdk.services.ssm.model.GetParametersByPathRequest;
 import software.amazon.awssdk.services.ssm.model.GetParametersByPathResponse;
+import software.amazon.awssdk.services.ssm.model.Parameter;
 
+import java.util.List;
 import java.util.Properties;
 
+import static io.netty.util.internal.StringUtil.isNullOrEmpty;
 import static java.util.Objects.requireNonNull;
 import static software.amazon.awssdk.core.regions.Region.EU_CENTRAL_1;
 import static software.amazon.awssdk.services.ssm.model.ParameterType.SECURE_STRING;
@@ -38,24 +41,26 @@ public class ParamStorePropertySourcePostProcessor implements BeanFactoryPostPro
 
     @Override
     public void postProcessBeanFactory(final ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        final Properties propertiesSource = new Properties();
 
-        final GetParametersByPathRequest.Builder requestBuilder = GetParametersByPathRequest
+        GetParametersByPathRequest.Builder requestBuilder = GetParametersByPathRequest
                 .builder()
                 .path(properties.getPath())
                 .recursive(true)
                 .withDecryption(true);
 
-        final Properties propertiesSource = new Properties();
+        final GetParametersByPathResponse firstPage = ssmClient.getParametersByPath(requestBuilder.build());
+        addParametersToPropertiesSource(propertiesSource, firstPage.parameters());
+        String nextToken = firstPage.nextToken();
 
-        final GetParametersByPathRequest request = requestBuilder.build();
-        final GetParametersByPathResponse result = ssmClient.getParametersByPath(request);
-        result.parameters().forEach(p -> {
-            final String name = p.name().substring(properties.getPath().length() + 1);
-            final String loggingValue = SECURE_STRING == p.type() ? "*****" : p.value();
-            LOG.info("Loaded '" + name + "' from ParametersStore, value='" + loggingValue + "', length=" + p.value().length());
-
-            propertiesSource.setProperty(name, p.value());
-        });
+        while (!isNullOrEmpty(nextToken)) {
+            final GetParametersByPathResponse nextPage = ssmClient.getParametersByPath(requestBuilder
+                    .nextToken(nextToken)
+                    .build()
+            );
+            addParametersToPropertiesSource(propertiesSource, nextPage.parameters());
+            nextToken = nextPage.nextToken();
+        }
 
         final ConfigurableEnvironment env = beanFactory.getBean(ConfigurableEnvironment.class);
         final MutablePropertySources propertySources = env.getPropertySources();
@@ -64,6 +69,16 @@ public class ParamStorePropertySourcePostProcessor implements BeanFactoryPostPro
         } else {
             propertySources.addFirst(new PropertiesPropertySource(PARAMETER_STORE_PROPERTY_SOURCE, propertiesSource));
         }
+    }
+
+    private void addParametersToPropertiesSource(final Properties propertiesSource, final List<Parameter> parameters) {
+        parameters.forEach(p -> {
+            final String name = p.name().substring(properties.getPath().length() + 1);
+            final String loggingValue = SECURE_STRING == p.type() ? "*****" : p.value();
+            LOG.info("Loaded '" + name + "' from ParametersStore, value='" + loggingValue + "', length=" + p.value().length());
+
+            propertiesSource.setProperty(name, p.value());
+        });
     }
 
     @Override
