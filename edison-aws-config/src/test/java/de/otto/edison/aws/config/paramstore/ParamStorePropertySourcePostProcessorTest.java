3cb30013c1631fcad17fe01f70f1172f1fd68ec6
mpaulsen <malte.paulsen@ottogroup.com>
2018-04-10 13:57:59 +0200

    add test for ssm paging

--- edison-aws-config/src/test/java/de/otto/edison/aws/config/paramstore/ParamStorePropertySourcePostProcessorTest.java
+++ edison-aws-config/src/test/java/de/otto/edison/aws/config/paramstore/ParamStorePropertySourcePostProcessorTest.java
@@ -5,19 +5,20 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.*;
+import org.springframework.mock.env.MockEnvironment;
 import software.amazon.awssdk.services.ssm.SSMClient;
 import software.amazon.awssdk.services.ssm.model.GetParametersByPathRequest;
 import software.amazon.awssdk.services.ssm.model.GetParametersByPathResponse;
 import software.amazon.awssdk.services.ssm.model.Parameter;
+import software.amazon.awssdk.services.ssm.model.ParameterMetadata;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
@@ -79,5 +80,46 @@ public class ParamStorePropertySourcePostProcessorTest {
         // then
         verify(ssmClient).getParametersByPath(eq(expectedRequest));
         verify(propertySourcesMock).addFirst(parameterStorePropertySource);
+    }
+
+    @Test
+    public void shouldReadPropertiesFromSsmWithPaging() {
+        // given
+        final GetParametersByPathResponse firstPage = mock(GetParametersByPathResponse.class);
+        final List<Parameter> parametersFirstPage = new ArrayList<>();
+        parametersFirstPage.add(Parameter.builder().name("/the/path/param1").value("val1").build());
+        when(firstPage.parameters()).thenReturn(parametersFirstPage);
+        when(firstPage.nextToken()).thenReturn("firstPageNextToken");
+        GetParametersByPathRequest firstRequest = GetParametersByPathRequest.builder()
+                .path("/the/path")
+                .withDecryption(true)
+                .recursive(true)
+                .nextToken(null)
+                .build();
+        when(ssmClient.getParametersByPath(eq(firstRequest))).thenReturn(firstPage);
+
+        final GetParametersByPathResponse secondPage = mock(GetParametersByPathResponse.class);
+        final List<Parameter> parametersSecondPage = new ArrayList<>();
+        parametersSecondPage.add(Parameter.builder().name("/the/path/param2").value("val2").build());
+        when(secondPage.parameters()).thenReturn(parametersSecondPage);
+        when(secondPage.nextToken()).thenReturn(null);
+        GetParametersByPathRequest secondRequest = GetParametersByPathRequest.builder()
+                .path("/the/path")
+                .withDecryption(true)
+                .recursive(true)
+                .nextToken("firstPageNextToken")
+                .build();
+        when(ssmClient.getParametersByPath(eq(secondRequest))).thenReturn(secondPage);
+
+        final ConfigurableEnvironment envMock = new MockEnvironment();
+        when(beanFactory.getBean(ConfigurableEnvironment.class)).thenReturn(envMock);
+
+        // when
+        postProcessor.postProcessBeanFactory(beanFactory);
+
+        // then
+        PropertySource<?> propertySource = envMock.getPropertySources().get("parameterStorePropertySource");
+        assertThat(propertySource.getProperty("/the/path/param1"), is("val1"));
+        assertThat(propertySource.getProperty("/the/path/param2"), is("val2"));
     }
 }
