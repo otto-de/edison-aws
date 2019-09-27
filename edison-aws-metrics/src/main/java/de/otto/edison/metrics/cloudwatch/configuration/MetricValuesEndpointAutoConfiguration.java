255521de52102e20a3eb4516859917309f7100eb
fouquetp <peter.fouquet@ottogroup.com>
2019-09-27 15:23:43 +0200

    Add ConditionalOnProperty(prefix = "management.metrics.export.cloudwatch"

--- edison-aws-metrics/src/main/java/de/otto/edison/metrics/cloudwatch/configuration/MetricValuesEndpointAutoConfiguration.java
+++ edison-aws-metrics/src/main/java/de/otto/edison/metrics/cloudwatch/configuration/MetricValuesEndpointAutoConfiguration.java
@@ -27,6 +27,7 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -39,6 +40,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConditionalOnClass(Timed.class)
 @ConditionalOnEnabledEndpoint(endpoint = MetricValuesEndpoint.class)
+@ConditionalOnProperty(prefix = "management.metrics.export.cloudwatch", name = "namespace")
 @AutoConfigureAfter({ MetricsAutoConfiguration.class, CompositeMeterRegistryAutoConfiguration.class })
 public class MetricValuesEndpointAutoConfiguration {
 
