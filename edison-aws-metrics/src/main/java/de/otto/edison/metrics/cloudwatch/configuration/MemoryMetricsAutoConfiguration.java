0802847d85d67400dd57215f0992926e7dd9fc90
fouquetp <peter.fouquet@ottogroup.com>
2019-09-27 13:22:01 +0200

    Build plain metrics for tagged Meters

---
+++ edison-aws-metrics/src/main/java/de/otto/edison/metrics/cloudwatch/configuration/MemoryMetricsAutoConfiguration.java
@@ -0,0 +1,20 @@
+package de.otto.edison.metrics.cloudwatch.configuration;
+
+import de.otto.edison.metrics.cloudwatch.MemoryUsageMetric;
+import io.micrometer.cloudwatch2.CloudWatchMeterRegistry;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@ConditionalOnProperty(prefix = "management.metrics.export.cloudwatch", name = "namespace")
+@ConditionalOnClass({ CloudWatchMeterRegistry.class })
+public class MemoryMetricsAutoConfiguration {
+    @Bean
+    @ConditionalOnProperty(value = "management.metrics.export.cloudwatch.enabled", matchIfMissing = true)
+    public MemoryUsageMetric memoryUsageMetric(MeterRegistry meterRegistry) {
+        return new MemoryUsageMetric(meterRegistry);
+    }
+}
