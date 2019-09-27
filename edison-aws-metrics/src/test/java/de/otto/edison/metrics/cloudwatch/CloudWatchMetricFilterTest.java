7835e8d4b9132c2126de6f5725d73b1d67aaf631
fouquetp <peter.fouquet@ottogroup.com>
2019-09-27 11:55:22 +0200

    Build plain metrics for tagged Meters

--- edison-aws-metrics/src/test/java/de/otto/edison/metrics/cloudwatch/CloudWatchMetricFilterTest.java
+++ edison-aws-metrics/src/test/java/de/otto/edison/metrics/cloudwatch/CloudWatchMetricFilterTest.java
@@ -49,4 +49,20 @@ public class CloudWatchMetricFilterTest {
         assertThat(filterMetric.getId(), is(new Meter.Id("jvm.memory.max", Tags.of("By_Environment" , "live"), null, null, null)));
     }
 
+    @Test
+    public void shouldCreateCloudwatchMetricWithTags() {
+        // given
+        final Tags tags = Tags.of("area", "heap", "id", "PS Eden Space");
+        final Meter.Id meterId = new Meter.Id("jvm.memory.max", tags, null, null, null);
+        final Measurement measurement = new Measurement(() -> 1.0D, Statistic.COUNT);
+        final List<Measurement> measurementList = Collections.singletonList(measurement);
+        final Meter metric = new DefaultMeter(meterId, Meter.Type.COUNTER, measurementList);
+
+        // when
+        final Meter filterMetric = cloudWatchMetricFilter.filter(metric);
+
+        // then
+        assertThat(filterMetric.getId(), is(new Meter.Id("jvm.memory.max.heap.psEdenSpace", Tags.of("By_Environment" , "live"), null, null, null)));
+    }
+
 }
